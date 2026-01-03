c49eee01b8a6bd39df35329ab5774e91eb110b87
A.karthick <karthicka293@gmail.com>
2026-01-03 18:04:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.messages import SystemMessage, HumanMessage
 
 # ⚠️ Replace with your actual API key
-GOOGLE_API_KEY = "AIzaSyCOBezqw9KgRqyHe7Dpp1yND0b6HFINH8I"
 
 # Initialize LLM with API key
 llm = ChatGoogleGenerativeAI(
@@ -50,4 +49,5 @@ for msg in st.session_state.conversation_history:
     else:
         with st.chat_message("assistant"):
             st.write(f"Sana: {msg.content}")
+
 
